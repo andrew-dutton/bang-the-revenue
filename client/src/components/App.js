e8db134302f07ef7a2b1a8a5c28a0cbc8f6a9582
Andrew Dutton <andrew@dutton.pro>
2019-06-03 18:56:38 +1000

    fix dashboard

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,9 +3,9 @@ import { BrowserRouter, Route } from 'react-router-dom'
 import { connect } from 'react-redux'
 import * as actions from '../actions'
 import Landing from './Landing'
+import Dashboard from './Dashboard'
 
 import Header from './Header'
-const Dashboard = () => <h2>Dashboard</h2>
 
 
 class App extends Component {
---
+++ client/src/components/Dashboard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>Dashboards</h1>
+    </div>
+  )
+}
+
+export default Dashboard
