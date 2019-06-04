26dd357c3ca3962a9ea85e02d04d5295eb0b2784
Andrew Dutton <andrew@dutton.pro>
2019-06-04 19:05:02 +1000

    dashboard route

--- index.js
+++ index.js
@@ -24,16 +24,22 @@ app.use(
 app.use(passport.initialize())
 app.use(passport.session())
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+require('./routes/authroutes')(app)
 
+//Configuration for Express to behave correctly in production environment
+if (process.env.NODE_ENV === 'production') {
+  //First - Making sure express will serve production assets - main.js, main.css, etc
+  app.use(express.static('client/build'));
+  //Second -Express will serve up the index.html file if it doesn't recognize the route
   const path = require('path');
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   });
 };
 
+//Dynamically figures out which port to listen to. If one hasn't been defined then by default use 5000
 const PORT = process.env.PORT || 5000
 
+//Instructs Express to tell Node to listen to PORT
 app.listen(PORT);
 
