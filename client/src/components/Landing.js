ae20205372c588f1756b9136c6fade87e88e5d26
Andrew Dutton <andrew@dutton.pro>
2019-06-09 16:10:54 +1000

    landing page update

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -4,7 +4,7 @@ const Landing = () => {
   return (
     <div style={{ textAlign: 'center' }}>
       <h1>Powerful Revenue Analysis</h1>
-      <h3>Coming soon...</h3>
+      <h3>...under construction...</h3>
     </div>
   )
 }
