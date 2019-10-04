f04063234c5f4ca861f6ed3929693b078c1ae9ed
Andrew Dutton <andrew@dutton.pro>
2019-10-04 17:08:43 +1000

    add quick link to dashboard

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -5,6 +5,7 @@ const Landing = () => {
     <div style={{ textAlign: 'center' }}>
       <h1>Bang The Table Financial Analysis</h1>
       <h3>Under Construction</h3>
+      <h5>Jump to <a href="/dashboard">Dashboard</a></h5>
     </div>
   )
 }
