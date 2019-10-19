f8f5631e8c776cc54f41a91de38b9fbbe9c9d52b
Andrew Dutton <andrew@dutton.pro>
2019-10-19 21:05:33 +1100

    spelling mistakes

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -668,31 +668,31 @@ class Dashboard extends Component {
             <h3 style={{ fontFamily: 'Titillium Web' }}>Other tasks, not ordered by priority</h3>
             <ul style={{ fontFamily: 'Titillium Web' }}>
               <li>Build out remaining dashboards</li>
-              <li>Create a privacy concious report of spending data that is meaningful and easy to report against revenue numbers. This is a very big job</li>
-              <li>Add mini graphs to each dashboard square where it would be better than just text</li>
+              <li>Create a privacy concious report of spending data that is meaningful and easy to report against revenue numbers. (This one is a big job)</li>
+              <li>Add mini graphs to each dashboard square for figures that could be better presented visually than just with text</li>
               <li>Add ability to change currency being displayed when currency flags are clicked (On Client Churn page only for now, but could be used on any dashboard)</li>
-              <li>Add functionality to display all data related to a specific client when their name is clicked on any table in which it appears</li>
-              <li>Time travel back to specific months on Active Licence Graph and Recurring Revenue Graph (same as currently available on Client Churn graph)</li>
-              <li>Non Recurring Revenue dashboard build out - use this to analyse sales for each product family</li>
+              <li>Add functionality to display all data related to a specific client when their name is clicked on any table in which they appear</li>
+              <li>Time travel back to specific months on Active Licence and Recurring Revenue Graphs (same as currently available on Client Churn Graph)</li>
+              <li>Non Recurring Revenue dashboard build - use this to analyse sales for each product family</li>
               <li>Set up Cost of Acquiring Clients Dashboard (CAC) - showing total marketing spend vs converted new leads (by month/qtr/annual)</li>
-              <li>EIQ dashboard build out - Show list of current EIQ licences and show their added value relative to client's EHQ licence</li>
+              <li>EIQ dashboard build - Display list of current EIQ licences and show their added value relative to client's EHQ licence</li>
               <li>LTV dashboard - there are a number of different formulas for calculating this. Create dynamic chart that can show results using different calculation methods</li>
               <li>Create board report option that automatically outputs a set of specific charts and table combinations in an exportable format</li>
-              <li>Toggle to switch main dashboard to dislpay all figures for current quarter rather than just previous month(current view)</li>
-              <li>Track "Expansion": Increase in revenue when we sell more than just a renewal, or renew clients on higher tier EIQ package</li>
+              <li>Add a toggle button on main dashboard that switches the dislpay of all figures to show current quarter rather than just previous month(current view)</li>
+              <li>Track "Expansion": Increase in revenue when when a client renews, we sell more than their previous licence, or if renew clients on to a higher EIQ package</li>
               <li>Toggle to switch main dashboard to dislpay all figures in any chosen currency</li>
 
             </ul>
             <h3 style={{ fontFamily: 'Titillium Web' }}>Non data related site improvements</h3>
             <ul style={{ fontFamily: 'Titillium Web' }}>
-              <li>Add basic invoice search page. Not as a dashboard. Link to this in will be in the header</li>
-              <li>Need a "Loading" icon for when toggling options take a while. A lot of number crunching in Client Churn causing some lag</li>
-              <li>Consider changing layout or colour scheme. Maybe make things smaller, or use more width of screen</li>
+              <li>Add basic invoice search page. Not as a dashboard. Link to this will be in the header</li>
+              <li>Need a "Loading" icon for when toggling options take a while to reload. A lot of number crunching in Client Churn causing some lag</li>
+              <li>Consider changing layout. Maybe make things smaller, or use more width of screen</li>
               <li>Add current forex rates on main screen</li>
               <li>Create funcionality for people to make comments</li>
               <li>Create ability to export tables to a google sheet</li>
               <li>Templates to create printer/PDF friendly nicely formatted reports</li>
-              <li>code should be refactored to make it more dry</li>
+              <li>Code should be refactored to make it more dry</li>
             </ul>
           </Segment>
 
