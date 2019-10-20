cc478a7ed7ad47755412edd0db2d02b95d3f1f58
Andrew Dutton <andrew@dutton.pro>
2019-10-20 11:21:03 +1100

    restyle landing page

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,17 +1,19 @@
 import React from 'react'
-import { Image, Grid, Button } from 'semantic-ui-react'
+import { Image } from 'semantic-ui-react'
 import btt from './dashboard/btt.png'
+import { Segment } from 'semantic-ui-react'
 
 const Landing = () => {
   return (
-    <div style={{ textAlign: 'center', paddingBottom: 400 }}>
-
-      <div style={{ textAlign: 'center' }}>
-        <a href="/dashboard">
-          <Image inline verticalAlign size={'huge'} src={btt} />
-        </a>
-        <h1 style={{ fontFamily: 'Titillium Web', fontSize: 75 }}>Financial Analysis</h1>
-      </div>
+    <div style={{ textAlign: 'center', paddingBottom: 400, paddingTop: 20 }}>
+      <Segment style={{ width: 1079, height: 800 }}>
+        <h1 style={{ fontFamily: 'Titillium Web', fontSize: 75, color: "#5E6468" }}>Financial Analysis</h1>
+        <div style={{ textAlign: 'center', paddingTop: 60 }}>
+          <a href="/dashboard">
+            <Image inline verticalAlign size={'large'} src={btt} />
+          </a>
+        </div>
+      </Segment>
     </div>
   )
 }
